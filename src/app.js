444778da6d41d870de797e3d2b71eaf20ae452ed
unknown <jeduclosson@gmail.com>
2024-03-06 16:52:32 -0300

    proyecto sin error

--- src/app.js
+++ src/app.js
@@ -6,17 +6,31 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
+
+
+
 app.get("/", async (req, res) => {
     const {limit} = req.query;
+    // Debe ir adentro del get porque utilizo products para el params, si carga uno solo es el que queda para el slice
+    let products = await PM.getProduct();
     
-    let users = await PM.getProduct();
-   
     if (limit) {
-       users = users.slice(0, limit);
+       products = products.slice(0, limit);
+       
     }
-        
-    res.send(users);
+   
+    res.send(products);
 });  
+
+app.get('/:id', async (req, res) => {
+    
+    let productId = req.params.id;
+    // Convierto el tipo para que no haya problemas en ProductManager con el ===
+    const products = await PM.getProductbyId(parseInt(productId));
+    res.send({products});
+});
+
+
 const PORT = 8080;
 
 app.listen(PORT, () => {
